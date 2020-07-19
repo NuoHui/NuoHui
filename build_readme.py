4e45c45f0baba44ac3ceccc120d5b1ef530c6a24
NuoHui <xyz1657413883@gmail.com>
2020-07-19 23:57:45 +0800

    feat: test py script

--- build_readme.py
+++ build_readme.py
@@ -161,9 +161,10 @@ if __name__ == "__main__":
     )
     project_releases.open("w").write(project_releases_content)
 
-    code_time_text = "\n```text\n"+fetch_code_time().text+"\n```\n"
+    # code_time_text = "\n```text\n"+fetch_code_time().text+"\n```\n"
 
-    rewritten = replace_chunk(rewritten, "code_time", code_time_text)
+    # rewritten = replace_chunk(rewritten, "code_time", code_time_text)
+    print(rewritten)
 
     # doubans = fetch_douban()[:5]
 
